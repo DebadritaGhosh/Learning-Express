57c7ae7108b8a33d300cdd147a7a32bc9184b9a6
Debadrita Ghosh <debadrita.ghosh2010@gmail.com>
2021-10-07 12:48:54 +0530

    rendered html file

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-
+//Route
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("<h1>Hello Express</h1>");
 });
+//rendering HTML file
+app.get("/web", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
 
+//Server
 app.listen(PORT, () => { console.log("Running......."); });
