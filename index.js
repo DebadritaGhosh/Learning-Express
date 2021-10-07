b1685d046a8a34bec2b9c44fbbde15f0058e6e43
Debadrita Ghosh <debadrita.ghosh2010@gmail.com>
2021-10-07 13:06:46 +0530

    Rendering JSON

--- index.js
+++ index.js
@@ -15,6 +15,36 @@ app.get("/web", (req, res) => {
 //Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Json
+
+const members = [
+    {
+        id: 1,
+        name: "Debadrita Ghosh",
+        email: "debadrita.ghosh2010@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Paushali Majumder",
+        email: "paushali@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Sourav Saha",
+        email: "sourav@gmail.com"
+    },
+    {
+        id: 4,
+        name: "Shreya Ghosh",
+        email: "shreya@gmail.com"
+    }
+]
+
+
+//rendering json
+app.get("/api/members", (req, res) => {
+    res.json(members);
+});
 
 //Server
 app.listen(PORT, () => { console.log("Running......."); });
