9ecaedf80b7cb968e2f4e7e40c464ee87d19c79e
Debadrita Ghosh <debadrita.ghosh2010@gmail.com>
2021-10-07 12:25:41 +0530

    first route created

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
 app.listen(PORT, () => { console.log("Running......."); });
