5fbe341caf704cf593baffbc433ae77503c04d12
Debadrita Ghosh <debadrita.ghosh2010@gmail.com>
2021-10-20 12:58:55 +0530

    Day2 initial commit

---
+++ Day-2/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.get("/", (req, res) => { res.sendFile(path.resolve(__dirname) + "/index.html") });
+
+app.get("/about", (req, res) => { res.sendFile(path.resolve(__dirname) + "/about.html") });
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log("Hello Server!!"));
